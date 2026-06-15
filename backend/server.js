c2d2604b8131b712e50588a1502a457d9d744c97
UDAY KAMDI <udaykamdid@gmail.com>
2026-06-15 19:26:03 +0530

    Debug Mongo URI

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ app.use('/api/schedules', scheduleRoutes);
 app.use('/api/bins', binRoutes);
 
 // Connect to MongoDB
+console.log(process.env.MONGO_URI);
 mongoose.connect('process.env.MONGO_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true
